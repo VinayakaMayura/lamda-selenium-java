c2e63381ce390e8e8e73c2aae2c666299e5bae05
Varuna Srivastava <varunas@thoughtworks.com>
2018-08-23 18:17:06 +0530

    Added a test to open url

---
+++ src/test/java/tests/StartTests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class StartTests extends BaseTest {
+
+    @Test
+    public void verifyUrl(){
+     System.out.println("Varuna");
+    }
+}
